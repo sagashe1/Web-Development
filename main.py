7f5953a0428846afd2583e1fbd747bd94b56bc9a
sagashe1 <88682614+sagashe1@users.noreply.github.com>
2022-08-04 04:13:41 +0000

    added home.html

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask  # library for creating web page
+from flask import Flask,render_template  # library for creating web page
 app = Flask(__name__)  # Created Flask app
 print(__name__) # when we run code on python, the __name__ is given __main__
 @app.route('/') # we can give the link of the URL 
 def hello_world():
-  return "Hello World!!"
+  return render_template('home.html')
 if __name__ == "__main__":
   print("Inside if")
   app.run(host='0.0.0.0',debug = True)
